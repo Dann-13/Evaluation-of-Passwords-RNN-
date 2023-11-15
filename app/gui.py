6b929699381779d328a1c265fe028a1ea0bba71c
Dann-13 <dannyelgomezz.13@gmail.com>
2023-11-14 20:10:37 -0500

    Usando modelo con la interfaz

--- app/gui.py
+++ app/gui.py
@@ -1,9 +1,18 @@
 import tkinter as tk
-
-
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 
 class AppGUI:
     def __init__(self):
+         # Cargar el modelo entrenado
+        self.modelo_cargado = load_model('/home/dan-dev/Documents/Proyectos_Personales/Proyects_Python/ProyectoIAContraseñas/data/modelo_RNN.h5')
+
+        # Cargar el tokenizer desde el archivo JSON
+        tokenizer_json = open('/home/dan-dev/Documents/Proyectos_Personales/Proyects_Python/ProyectoIAContraseñas/data/tokenizer.json', 'r').read()
+        self.tokenizer = tokenizer_from_json(tokenizer_json)
+        
         self.root = tk.Tk()
         self.root.title("Mi Aplicación")
         self.root.geometry("900x500")
@@ -49,10 +58,24 @@ class AppGUI:
         self.resultado_texto.config(state=tk.DISABLED) 
     def evaluar_contrasena(self):
         contrasena_prueba = self.entrada_contraseña.get()
+        # Tokenización de la contraseña de prueba
+        contrasena_prueba_seq = self.tokenizer.texts_to_sequences([contrasena_prueba])
+        contrasena_prueba_seq = pad_sequences(contrasena_prueba_seq, maxlen=35)  # Usa la longitud máxima que determinaste
+
+        # Realizar la predicción utilizando el modelo cargado
+        prediccion = self.modelo_cargado.predict(contrasena_prueba_seq)
+
+        # Obtener la clasificación numérica
+        clasificacion_numerica = np.argmax(prediccion, axis=1)
+
+        # Obtener la probabilidad de cada clase
+        probabilidades = prediccion[0]
+        # Mostrar el resultado en el widget de texto
+        resultado = f"Clasificación numérica de la contraseña: {clasificacion_numerica[0]}\nProbabilidades de cada clase (Débil, Segura, Bastante Segura): {probabilidades}\n"
         self.resultado_texto.config(state=tk.NORMAL)  # Habilitar la edición del widget de texto
         self.resultado_texto.delete(1.0, tk.END)  # Limpiar el contenido anterior
-        self.resultado_texto.insert(tk.END, f"La contraseña ingresada es segura: {contrasena_prueba}\n")
+        self.resultado_texto.insert(tk.END, resultado)
         self.resultado_texto.config(state=tk.DISABLED)  # Deshabilitar la edición del widget de texto
-        
+
     def run(self):
         self.root.mainloop()
